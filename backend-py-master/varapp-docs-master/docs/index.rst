aea2841501bb32c5e21779a0c20f437b36c07a5f
Varada P P <varadaviswanathan27@gmail.com>
2026-03-31 11:40:06 +0530

    Add files via upload

---
+++ backend-py-master/varapp-docs-master/docs/index.rst
@@ -0,0 +1,23 @@
+.. varapp documentation master file, created by
+   sphinx-quickstart on Tue May 19 14:51:57 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Varapp's documentation!
+==================================
+
+Updated last: 24th August 2016
+
+Contents
+''''''''
+
+.. toctree::
+   :maxdepth: 2
+
+   src/about
+   src/data_preparation
+   src/deployment
+   src/usage
+   src/acknowledgements
+   src/contact
+
---
+++ backend-py-master/varmed/settings/settings.py
@@ -0,0 +1,41 @@
+from varmed.settings.base import *
+import os, sys, logging
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(message)s')
+logging.info("\n-----------  << RESTART >> -----------\n")
+logging.info("Settings file: " + os.path.basename(__file__))
+
+## General
+DEBUG = True                        # If True, will return full tracebacks when an HTTP error is produced. Set to False in production!
+HOST = 'localhost'                  # Hostname, as returned by "hostname --fqdn" on unix.
+BASE_URL = 'http://localhost:8000'  # URL to serve the app to.
+SECRET_KEY = 'K6QKN6C2xtcl.'        # Used to generate authentication tokens. Change it and keep it secret!
+
+## Email: SMTP server configuration
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_FROM = 'varapp@varapp.vital-it.ch'
+
+## Gemini dbs
+GEMINI_DB_PATH = './resources/db'   # Path to Gemini databases container
+WARMUP_STATS_CACHE = True           # Generate stats cache for all active dbs at startup
+WARMUP_GENOTYPES_CACHE = True       # Generate genotypes cache for all active dbs at startup
+
+## Users db
+DB_USERS = 'users_db'               # Name of the main database, that stores sessions, db connections etc.
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PWD = 'pwd'
+MYSQL_PORT = ''
+
+## Adds the users_db to DATABASES
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': DB_USERS,
+    'USER': MYSQL_USER,
+    'PASSWORD': MYSQL_PWD,
+    'HOST': MYSQL_HOST,
+    'PORT': MYSQL_PORT,
+}
+
+logging.info("--------------------------------------")
+
